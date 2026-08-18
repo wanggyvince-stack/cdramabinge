209d9549f4c5ccef130148a59ec74fedbfaa0fa7
wanggyvince-stack <2926455353+wanggyvince-stack@users.noreply.github.com>
2026-08-18 17:14:29 +0800

    feat: add Microsoft Clarity tracking (y48fxh3upm)

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -85,6 +85,16 @@ export default async function LocaleLayout({
             gtag('config', 'G-DRJPX6Z846');
           `}
         </Script>
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "y48fxh3upm");
+          `}
+        </Script>
         {/* Global JSON-LD: WebSite + SearchAction */}
         <script
           type="application/ld+json"
